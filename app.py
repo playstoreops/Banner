8b58d18b5f98981c7720f57323154bf320af57ac
playstoreops <145094756+playstoreops@users.noreply.github.com>
2026-04-24 12:16:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ INFO_API_URL = "https://infofull.vercel.app/get"
 BASE64 = "aHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL1NoYWhHQ3JlYXRvci9pY29uQG1haW4vUE5H"
 info_URL = base64.b64decode(BASE64).decode('utf-8')
 
-FONT_FILE = "arial_unicode_bold.otf"
+FONT_FILE = "unifont_upper_sample-17.0.04.otf"
 FONT_CHEROKEE = "unifont_upper_sample-17.0.04.otf"
 
 client = httpx.AsyncClient(
